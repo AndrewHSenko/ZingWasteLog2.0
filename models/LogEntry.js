79166264c2fcd483736488a3a2c5cb30e62241e7
Andrew Senkowski <51846520+AndrewHSenko@users.noreply.github.com>
2025-12-21 23:13:50 -0800

    Created models

---
+++ models/LogEntry.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const LogEntrySchema = new mongoose.Schema({
+    entererName: {
+        type: String,
+        required: [true, 'Please enter your name.'],
+        trim: true, // trims surrounding whitespace
+    },
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: [true, 'Please enter the name of the ingredient/product.'] 
+    },
+    productQuantity: {
+        type: Number, // Will handle non-numeric entries with middleware
+        required: [true, 'Please enter the amount wasted.'],
+        min: [0.001, 'Must be more than 0, duh.'] // Default, will be handled pre-emptively with middleware
+    }},
+    {timestamps : true} // Records createdAt and updatedAt
+)
+
+module.exports = mongoose.model('LogEntry', LogEntrySchema)
---
+++ models/Product.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Must provide name!'],
+        trim: true, // trims surrounding whitespace
+        minlength: [2, 'Name must be more than one letter unless we\'re selling alphabet soup!']
+    },
+    quantityType: {
+        type: String,
+        required: [true, 'Must provide a measurement type!'],
+        trim: true
+    }},
+    {timestamps : true} // Records createdAt and updatedAt
+)
+
+module.exports = mongoose.model('Product', ProductSchema)
