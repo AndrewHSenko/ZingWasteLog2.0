1f59a5cdada0b02dc65efc0737b1a983adbfc896
Andrew Senkowski <51846520+AndrewHSenko@users.noreply.github.com>
2026-05-20 11:31:08 -0400

    Fixed header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,34 +1,31 @@
-import { useHeaderHeight } from '../HeaderHeightContext.jsx'
 import { Link } from 'react-router'
 
 import z_logo from '../assets/z_logo.png'
 
 const Header = () => {
-  const { headerRef } = useHeaderHeight()
 
   return (
-    <header ref={headerRef} className="sticky-top z-2 bg-primary">
-      <nav className="navbar navbar-expand-lg">
-        <div className="container-fluid">
-            <div className="d-flex justify-content-between align-items-center mx-3 flex-nowrap w-100">
-                <Link to="/" className="navbar-brand" aria-current="page">
-                    <img src={z_logo} alt="Eatery logo" className="w-75"/>
-                </Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-            </div>
-            <div className="collapse navbar-collapse w-100 pt-2 justify-content-end pe-lg-5 me-lg-5" id="navbar">
-                <div className="navbar-nav gap-2 gap-lg-5 flex-row">
-                    <Link to="/" className="btn border-1 border-dark mx-auto d-lg-block d-none" aria-current="page"><p className="fs-1">Home</p></Link>
-                    <Link to="entries" className="btn border-1 border-dark mx-auto">All Entries</Link>
-                    <Link to="/" className="btn border-1 border-dark mx-auto d-lg-none" aria-current="page">Home</Link>
-                    <Link to="items" className="btn border-1 border-dark mx-auto">All Items</Link>
+    <header className="sticky-top z-3 bg-primary">
+            <nav className="navbar navbar-expand-lg w-100 py-0">
+                <div className="container-fluid mx-4">
+                  <div className="d-flex flex-column align-items-center justify-content-center ms-md-4">
+                    <Link to="/" className="navbar-brand me-0 py-md-1 py-2" aria-current="page">
+                          <img src={z_logo} alt="Zing logo" className="img-fluid logo"/>
+                      </Link>
+                  </div>
+                  <button className="navbar-toggler ms-auto me-md-5" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                      <span className="navbar-toggler-icon"></span>
+                  </button>
+                  <div className="collapse navbar-collapse w-100 pt-1" id="navbar">
+                      <div className="navbar-nav gap-2 me-lg-5 gap-lg-5 d-flex justify-content-center ms-auto">
+                          <Link to="/entries" className="btn" aria-current="page"><h5 className="lead">See all entries</h5></Link>
+                          <Link to="/items" className="btn" aria-current="page"><h5 className="lead">See categories</h5></Link>
+                          <Link to="/" className="btn"><h5 className="lead fw-bold">Home</h5></Link>
+                      </div>
+                  </div>
                 </div>
-            </div>
-        </div>
-      </nav>
-    </header>
+            </nav>
+        </header>
   )
 }
 
--- frontend/src/pages/MainLayout.jsx
+++ frontend/src/pages/MainLayout.jsx
@@ -1,17 +1,14 @@
 import { Outlet } from 'react-router'
-import { HeaderHeightProvider } from '../HeaderHeightContext.jsx'
 import Header from '../components/Header.jsx'
 
 const MainLayout = ({ children }) => {
   return (
-    <HeaderHeightProvider>
         <div className="container-fluid d-flex flex-column p-0 min-vh-100">
             <Header />
             <main className="flex-grow-1">
                 <Outlet />
             </main>
         </div>
-    </HeaderHeightProvider>
   )
 }
 
